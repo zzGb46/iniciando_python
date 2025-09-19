aa0bd417b7ffab87e5fc58d78fa8c812017b2fac
GABRIEL MATHEUS ALVES DO NASCIMENTO SOUSA <gabriel.mansousa@senacsp.edu.br>
2025-09-19 15:24:08 -0300

    iniciando python

---
+++ aula.py
@@ -0,0 +1,31 @@
+# print("helou")
+# print("estou no dba")
+# nome = input("Digite seu nome:")
+# print(nome)
+# nome = "gb"
+
+# print(f"meu nome é {nome}")
+
+# nota_1=int(input("Digite primeira nota")) 
+# nota_2 = int(input("Digite a segunda nota"))
+# nota_3 = int(input("Digite a terceira nota"))
+# nota_4 = int(input("Digite a quarta nota"))
+
+# media = (nota_1+nota_2+nota_3+nota_4)/4
+
+# # print(f"a media foi de: {media}")
+
+# if media >= 6:
+#     situacao = "aprovado"
+# else:
+#     situacao = "reprovado"
+
+#     print(f"a media foi {media}")
+#     print(f"a situação é {situacao}")
+
+def divisao(a,b):
+    resultado = a/b
+    return resultado
+
+resultado_divisao = divisao(3,3)
+print(resultado_divisao)
